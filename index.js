8683c1f0df424c08d3b436b3992cba55c61b3885
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-16 01:12:28 -0300

    se configuro el puerto

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import express from 'express'
 
-console.log('hola mundo')
+const app = express();
+app.set('port', process.env.PORT ||  4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'))  })
+  
