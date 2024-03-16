ada5031739b7b903621033c1260cbe1b0175cd4d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-16 02:45:00 -0300

    se creo la funcion para listar los colores

--- src/controllers/paletaColores.controllers.js
+++ src/controllers/paletaColores.controllers.js
@@ -1,9 +1,16 @@
 import Color from "../database/model/colores.js";
 
-export const listarColores = (req, res)=>{
-    console.log('desde listar colores');
-    res.send('enviar lista de tareas...');
-}
+export const listarColores = async (req, res) => {
+    try {
+      const colores = await Color.find();
+      res.status(200).json(colores);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "No se pudo encontrar la lista de colores",
+      });
+    }
+  };
 
 export const crearColor = async (req, res) => {
     try {
